58eae475ea288e3cafc94cfb57f83c52b08951c6
eenzzi <142785172+eenzzi@users.noreply.github.com>
2024-05-23 18:17:29 +0900

    [Silver IV] Title: ATM, Time: 152 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,51 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        System.out.println(time(n, arr));
+
+        br.close();
+    }
+
+    private static int time(int n, int[] arr) {
+
+        int time = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                time += arr[j];
+            }
+        }
+
+        return time;
+    }
+
+
+}

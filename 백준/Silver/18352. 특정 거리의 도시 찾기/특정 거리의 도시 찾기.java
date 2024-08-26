eb9e85216148afbcc4f5efdf3ae9c9ad73811e60
eenzzi <142785172+eenzzi@users.noreply.github.com>
2024-08-26 15:13:07 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 948 ms, Memory: 257736 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,64 +1,80 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-import java.util.StringTokenizer;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
+
+	static ArrayList<Integer>[] arr;
+	static int[] dis;
+	static ArrayList<Integer> result;
+	static int N, M, K, X;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int cities = Integer.parseInt(st.nextToken());
-		int roads = Integer.parseInt(st.nextToken());
-		int distance = Integer.parseInt(st.nextToken());
-		int start = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		X = Integer.parseInt(st.nextToken());
+		arr = new ArrayList[N + 1];
+		result = new ArrayList<>();
 		
-		int[] visited = new int[cities+1];
-		
-		Map<Integer, ArrayList<Integer>> m = new HashMap<>();
-		Queue<Integer> que = new LinkedList<>();
-		
-		for (int i = 0; i <roads; i++) {
-			st = new StringTokenizer(br.readLine());
-			int k = Integer.parseInt(st.nextToken());
-			ArrayList<Integer> val;
-			if (m.containsKey(k)) {
-				val = m.get(k);
-			}
-			else {
-				val = new ArrayList<>();
-			}
-			val.add(Integer.parseInt(st.nextToken()));
-			m.put(k, val);
+		for (int i = 0; i <= N; i++) {
+			arr[i] = new ArrayList<>();
 		}
 		
-		que.add(start);
-		while (!que.isEmpty()) {
-			int key = que.poll();
-			if (visited[key] == distance) break;
-			if (m.get(key) == null) continue;
-			for (Integer a : m.get(key)) {
-				if (visited[a] == 0) {
-					visited[a] = visited[key] + 1;
-					que.add(a); 
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			arr[a].add(b); 
+		}
+		
+		bfs();
+
+		if (result.isEmpty()) {
+			System.out.println(-1);
+		} else {
+			Collections.sort(result);
+			for (int i : result) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+	
+	private static void bfs() {
+		Queue<Integer> q = new LinkedList<>();
+		dis = new int[N + 1];
+		
+		for (int i = 0; i <= N; i++) {
+			dis[i] = -1;
+		}
+
+		q.add(X);
+		dis[X]++;
+		
+		while(!q.isEmpty()) {
+			
+			int node = q.poll();
+			
+			if (dis[node] > K) {
+				continue;
+			}
+			
+			if (dis[node] == K) {
+				result.add(node);
+			}
+			
+			for (int i : arr[node]) {
+				if (dis[i] == -1) {
+					q.add(i);
+					dis[i] = dis[node] + 1;
 				}
 			}
 		}
-        visited[start] = 0;
-		
-		for (int i = 1; i <= cities; i++) {
-			if (visited[i] == distance) {
-				sb.append(i).append("\n");
-			}
-		}
-
-		System.out.println(sb.length() == 0 ? -1 : sb.toString());
 	}
+
 }

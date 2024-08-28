eee15cabcde5894715df85924cb3e41b87e989d8
eenzzi <142785172+eenzzi@users.noreply.github.com>
2024-08-29 00:04:24 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 182 ms, Memory: 19,992 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,32 +1,35 @@
-
 import java.util.Scanner;
 
 public class Solution {
 
-	public static void main(String[] args)  {
-		
-		//Scanner 제약
-		Scanner sc = new Scanner(System.in);
-		
-		int T = 10;
-		
-		for (int tc = 1; tc <= T; tc++) {
-			int num = sc.nextInt();
-			
-			int N = sc.nextInt();
-			int M = sc.nextInt();
-			
-			System.out.println("#" + tc + " " + recur(N, M));
-		}
+    static int N, M;
 
-	}
-	
-	private static int recur(int N, int M) {
-		if (M == 1) {
-			return N;
-		}
-		
-		return N * recur(N, M - 1);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        for (int tc = 1; tc <= 10; tc++) {
+            int t = sc.nextInt();
+            N = sc.nextInt();
+            M = sc.nextInt();
+
+            int result = pow(M, N);
+
+            System.out.println("#" + tc + " " + result);
+        }
+
+    }
+
+    private static int pow(int M, int N) {
+        if (M == 1) {
+            return N;
+        }
+
+        if (M % 2 == 0) {
+            int tmp = pow(M / 2, N);
+            return tmp * tmp;
+        } else {
+            int tmp = pow((M - 1) / 2, N);
+            return tmp * tmp * N;
+        }
+    }
 }

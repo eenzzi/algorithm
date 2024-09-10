861f4738ce3a53c47bd944ec5ad8f557a5677929
eenzzi <142785172+eenzzi@users.noreply.github.com>
2024-09-10 16:36:38 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 16656 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,63 +1,63 @@
-
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N, M;
-	static int[][] graph, depth;
-	static boolean[][] visited;
-	static Queue<int[]> q;
-	static int[] dx = {1, -1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
 
-	public static void main(String[] args) throws IOException {
+	static int[][] arr;
+	static int N, M;
+	static boolean[][] visited;
+	static int[] dr = { -1, 1, 0, 0 }; // 상 하 좌 우
+	static int[] dc = { 0, 0, -1, 1 };
+
+	public static void main(String[] args) throws Exception {
+//		System.setIn(new FileInputStream("src/평4/2178.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		graph = new int[N][M];
-		depth = new int[N][M];
+
+
+		String[] str1 = br.readLine().trim().split(" ");
+		N = Integer.parseInt(str1[0]);
+		M = Integer.parseInt(str1[1]);
+		arr = new int[N][M];
 		visited = new boolean[N][M];
-		
+
 		for (int i = 0; i < N; i++) {
-			String read = br.readLine();
+			String[] str = br.readLine().trim().split("");
 			for (int j = 0; j < M; j++) {
-				int a = read.charAt(j) - '0';
-				graph[i][j] = a;
+				arr[i][j] = Integer.parseInt(str[j]);
 			}
 		}
-		
-		q = new LinkedList<>();
-		depth[0][0] = 1;
-		
+
 		bfs();
-		
-		System.out.println(depth[N - 1][M - 1]);
-	}
-	
+		System.out.println(arr[N - 1][M - 1]);
+
+	}// end main
+
 	private static void bfs() {
-		q.add(new int[] {0, 0});
+		Queue<Integer> qR = new LinkedList<>();
+		Queue<Integer> qC = new LinkedList<>();
+
+		qR.offer(0);
+		qC.offer(0);
 		visited[0][0] = true;
-		
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-			int x = cur[0];
-			int y = cur[1];
-			
-			for (int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				
-				if (nx >= 0 && ny >= 0 && nx < N && ny < M && !visited[nx][ny] && graph[nx][ny] == 1) {
-					q.offer(new int[] {nx, ny});
-					depth[nx][ny] = depth[x][y] + 1;
-					visited[nx][ny] = true;
+
+		while (!qR.isEmpty()) {
+			int r = qR.poll();
+			int c = qC.poll();
+
+			for (int k = 0; k < 4; k++) {
+				int flagR = r + dr[k];
+				int flagC = c + dc[k];
+
+				if (flagR >= 0 && flagR < N && flagC >= 0 && flagC < M && arr[flagR][flagC] == 1
+						&& !visited[flagR][flagC]) {
+					qR.offer(flagR);
+					qC.offer(flagC);
+					visited[flagR][flagC] = true;
+					arr[flagR][flagC] = arr[r][c] + 1;
 				}
 			}
 		}

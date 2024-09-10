9b0057c55dd52facfa21158ea49a7df419377a40
eenzzi <142785172+eenzzi@users.noreply.github.com>
2024-09-10 17:00:29 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 15936 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine().trim());
+		
+		for (int tc = 1; tc <= T; tc++) {
+			String[] str1 = br.readLine().trim().split(" ");
+			
+			int A = Integer.parseInt(str1[0]);
+			int B = Integer.parseInt(str1[1]);
+			
+			sb.append("Case #").append(tc + ": ").append(A + B).append("\n");
+		}
+		
+		
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+
+	} // 메인 끝
+
+}

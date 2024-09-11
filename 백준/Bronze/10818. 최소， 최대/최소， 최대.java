7a2f0b73c98cf3d950442ce55560c737bda1c21b
eenzzi <142785172+eenzzi@users.noreply.github.com>
2024-09-11 17:36:55 +0900

    [Bronze III] Title: 최소, 최대, Time: 436 ms, Memory: 109844 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,47 +1,41 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
 
 public class Main {
 
-    static int n, min, max;
-    static int[] arr;
+	public static void main(String[] args) throws Exception {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine().trim());
+		
+		String[] str1 = br.readLine().trim().split(" ");
+		int[] nums = new int[N];
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(str1[i]);
+		}
+		
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < N; i++) {
+			max = Math.max(max, nums[i]);
+			min = Math.min(min, nums[i]);
+		}
+		
+		sb.append(min).append(" ").append(max);
+		
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        min = 1000000;
-        max = -1000000;
-
-        for (int i = 0; i < n; i++) {
-            if (min > arr[i]) {
-                min = arr[i];
-            }
-
-            if (max < arr[i]) {
-                max = arr[i];
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-        System.out.println(sb.append(min).append(" ").append(max));
-
-
-    }
-
-
+	} // 메인 끝
 
 }

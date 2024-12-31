273744c6a5de22341aa9d8034f7affa2761bf6c3
eenzzi <142785172+eenzzi@users.noreply.github.com>
2024-12-31 17:21:15 +0900

    [Silver I] Title: 신입 사원, Time: 740 ms, Memory: 297200 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -0,0 +1,47 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int T;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        T = Integer.parseInt(br.readLine());
+
+        for (int tc = 0; tc < T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] scores = new int[N + 1];
+
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                scores[a] = b;
+            }
+
+            int result = 0;
+            int limit = scores[1];
+
+            for (int i = 2; i <= N; i++) {
+
+                if (limit > scores[i]) {
+                    result++;
+                    limit = scores[i];
+                }
+            }
+
+            System.out.println(result + 1); // 1등
+
+        }
+    }
+
+}

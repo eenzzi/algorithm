9e37f447eac8be5738d92a51b2ba3b4629fe2be4
eenzzi <142785172+eenzzi@users.noreply.github.com>
2025-06-19 21:12:13 +0900

    [level 3] Title: 징검다리 건너기, Time: 23.69 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,48 +1,6 @@
-import java.util.*;
-
 class Solution {
-    
-    int[] stones;
-    int k;
-    
     public int solution(int[] stones, int k) {
-        
-        this.stones = stones;
-        this.k = k;
-        
-        int left = 1;
-        int right = 200000000;
         int answer = 0;
-        
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            
-            if (cross(mid)) { // 건널 수 있다면
-                answer = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        
         return answer;
-    }
-
-    public boolean cross(int value) {
-        int count = 0;
-        
-        for (int stone : stones) {
-            if (stone < value) { // 0
-                count++;
-                
-                if (count == k) {
-                    return false;
-                }
-            } else {
-                count = 0;
-            }
-        }
-        
-        return true;
     }
 }
